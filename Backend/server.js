61ce80617ec903c2f2940d057eefbc218c644f5e
ritikdevelop <hrithikrana216@gmail.com>
2024-11-30 23:12:51 +0530

    "Update main script in package.json to server.js and modify server.js to use environment variable for port and log port number on startup"

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,11 @@
 const http = require('http');
 const app = require('./app');
+const port = process.env.PORT || 3000;
 
 
 const server = http.createServer(app);
 
 
-server.listen(3000);
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
