4d935f75ac28e3523945b077704a903f945f43c4
ritikdevelop <hrithikrana216@gmail.com>
2024-12-04 22:41:04 +0530

    "Updated UserLogin component to include a form with email and password inputs and a login button."

--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const UserLogin = () => {
   return (
-    <div>UserLogin</div>
+    <div className=''>
+        <form>
+            <h3>What's your email</h3>
+            <input type="email" placeholder='Enter your email' required />
+            <h3>What's your password</h3>
+            <input type="password" placeholder='password' required />
+            <button>Login</button>
+        </form>
+    </div>
   )
 }
 
