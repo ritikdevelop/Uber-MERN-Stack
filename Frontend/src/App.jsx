db55fdc55b39786fd2080ec959db4911d1d7cb0a
ritikdevelop <hrithikrana216@gmail.com>
2024-12-06 22:32:44 +0530

    Created UserSignUp file and updated the code file for sign page.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import UserLogin from "./pages/UserLogin";
+import UserSignUp from "./pages/UserSignUp";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<UserLogin />} />
+        <Route path="/sign-p" element={<UserSignUp />} />
       </Routes>
     </div>
   );
