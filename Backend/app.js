b1ed3e98ab83094bccda16281da385746950f149
ritikdevelop <hrithikrana216@gmail.com>
2024-12-08 12:28:10 +0530

    Added user registration route and controller, Created user model with necessary fields and methods, Added user service for creating users, Added validation for user registration fields

--- Backend/app.js
+++ Backend/app.js
@@ -4,13 +4,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const connectToDB = require('./db/db');
+const userRoutes = require('./routes/user.routes');
 
 
 connectToDB();
 
 app.use(cors());
-
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 
@@ -19,6 +20,9 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
+
+app.use('/users', userRoutes);
+
 
 
 
--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,34 @@
-const userModel = require('../models/user.model');
-const userService = require('../services/user.service');
+const userModel = require("../models/user.model");
+const userService = require("../services/user.service");
+const { validationResult } = require("express-validator");
 
+/**
+//  * Register a new user
+//  * @function
+//  * @param {Object} req - The request object
+//  * @param {Object} res - The response object
+//  * @param {Function} next - The next middleware function
+ */
 module.exports.registerUser = async (req, res, next) => {
-    
-}
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+//   console.log(req.body);
+
+  const { FullName, email, password } = req.body;
+
+  const hashPassword = await userModel.hashPassword(password);
+
+  const user = await userService.createUser({
+    firstName: FullName.firstName,
+    lastName: FullName.lastName,
+    email,
+    password: hashPassword,
+  });
+
+  const token = user.generateAuthToken();
+
+  res.status(201).json({ user, token });
+};
