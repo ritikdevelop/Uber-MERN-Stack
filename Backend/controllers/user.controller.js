4ea9bde7c82491cbf1ef95acb8c7f36a6d081550
ritikdevelop <hrithikrana216@gmail.com>
2024-12-16 23:04:32 +0530

    `Refactor user controller: add fullname validation and minor improvements`

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -4,13 +4,17 @@ const { validationResult } = require('express-validator');
 const blackListTokenModel = require('../models/blacklistToken.model');
 
 module.exports.registerUser = async (req, res, next) => {
-
+    
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
     const { fullname, email, password } = req.body;
+
+    if (!fullname || !fullname.firstname || !fullname.lastname) {
+        return res.status(400).json({ message: 'Fullname is required with firstname and lastname.' });
+    }
 
     const isUserAlready = await userModel.findOne({ email });
 
@@ -30,12 +34,9 @@ module.exports.registerUser = async (req, res, next) => {
     const token = user.generateAuthToken();
 
     res.status(201).json({ token, user });
-
-
 }
 
 module.exports.loginUser = async (req, res, next) => {
-
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
@@ -63,17 +64,14 @@ module.exports.loginUser = async (req, res, next) => {
 }
 
 module.exports.getUserProfile = async (req, res, next) => {
-
     res.status(200).json(req.user);
-
 }
 
 module.exports.logoutUser = async (req, res, next) => {
     res.clearCookie('token');
-    const token = req.cookies.token || req.headers.authorization.split(' ')[ 1 ];
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
 
     await blackListTokenModel.create({ token });
 
     res.status(200).json({ message: 'Logged out' });
-
 }
