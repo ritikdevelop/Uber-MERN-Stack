3c8c1e751d66c74ff336b262eca2e61bf5d29381
ritikdevelop <hrithikrana216@gmail.com>
2025-01-05 20:39:02 +0530

    Refactor app.js: standardize string quotes and clean up code formatting

--- Backend/app.js
+++ Backend/app.js
@@ -1,20 +1,17 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const CookieParser = require('cookie-parser');  
-const connectToDB = require('./db/db');
-const userRoutes = require('./routes/user.routes');
-const captainRoutes = require('./routes/captain.routes');
+const CookieParser = require("cookie-parser");
+const connectToDB = require("./db/db");
+const userRoutes = require("./routes/user.routes");
+const captainRoutes = require("./routes/captain.routes");
 
-
-const crypto = require('crypto');
-const secret = crypto.randomBytes(32).toString('base64');
+const crypto = require("crypto");
+const secret = crypto.randomBytes(32).toString("base64");
 // console.log(secret);
 process.env.JWT_SECRET = secret;
-
-
 
 connectToDB();
 
@@ -23,18 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(CookieParser());
 
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.use('/users', userRoutes);
-app.use('/captains', captainRoutes);
-
-
-
+app.use("/users", userRoutes);
+app.use("/captains", captainRoutes);
 
 module.exports = app;
