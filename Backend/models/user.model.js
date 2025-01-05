54d4725c7f1bb6d28615e49bda3d0ce3018b2cd8
ritikdevelop <hrithikrana216@gmail.com>
2025-01-05 20:40:05 +0530

    Refactor user model: standardize string quotes and improve code formatting

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,49 +1,51 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const userSchema = new mongoose.Schema ({
-    FullName: {
-        firstName: {
-            type: String,
-            required: true,
-            minlength: [3, "First name must be at least 3 characters long"]
-        },
-        lastName: {
-            type: String,
-            minlength: [3, "Last name must be at least 3 characters long"]
-        }
+const userSchema = new mongoose.Schema({
+  FullName: {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: [3, "First name must be at least 3 characters long"],
     },
-    email: {
-        type: String,
-        unique: true,
-        minlength: [5, "Email must be at least 3 characters long"],
-        required: true,
-        match: [/.+\@.+\..+/, "Please fill a valid email address"]
+    lastName: {
+      type: String,
+      minlength: [3, "Last name must be at least 3 characters long"],
     },
-    password: {
-        type: String,
-        required: true,
-        select: false
-    },
-    socketId: {
-        type: String,
-    }
+  },
+  email: {
+    type: String,
+    unique: true,
+    minlength: [5, "Email must be at least 3 characters long"],
+    required: true,
+    match: [/.+\@.+\..+/, "Please fill a valid email address"],
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  socketId: {
+    type: String,
+  },
 });
 
 userSchema.methods.generateAuthToken = function () {
-    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
-    return token;
-}
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "24h",
+  });
+  return token;
+};
 
 userSchema.methods.comparePassword = async function (password) {
-    return await bcrypt.compare(password, this.password);
-}
+  return await bcrypt.compare(password, this.password);
+};
 
 userSchema.statics.hashPassword = async function (password) {
-    return await bcrypt.hash(password, 10);
-}
+  return await bcrypt.hash(password, 10);
+};
 
-const userModel = mongoose.model('User', userSchema);
+const userModel = mongoose.model("User", userSchema);
 
 module.exports = userModel;
